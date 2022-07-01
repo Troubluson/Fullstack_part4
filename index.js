7b1c9c38c8d354977f4930bc95bfb2afe3018e8f
Henrik Niskanen <h.niskanen99@gmail.com>
2022-07-02 01:44:51 +0300

    copied premade code for 4.1

--- index.js
+++ index.js
@@ -1,103 +1,43 @@
-require('dotenv').config();
-const express = require('express');
-const morgan = require('morgan');
-const cors = require('cors');
-const Person = require('./models/Person');
+const http = require('http')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
 
-const app = express();
+const blogSchema = new mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number
+})
 
-app.use(express.static('build'));
-app.use(cors());
-app.use(express.json());
+const Blog = mongoose.model('Blog', blogSchema)
 
-morgan.token('content', (req, res) => JSON.stringify(req.body));
+const mongoUrl = 'mongodb://localhost/bloglist'
+mongoose.connect(mongoUrl)
 
-// tiny + our defined "content"
-app.use(
-    morgan(
-        // eslint-disable-next-line max-len
-        ':method :url :status :res[content-length] - :response-time ms :content',
-    ),
-);
+app.use(cors())
+app.use(express.json())
 
-app.get('/info', (request, response, next) => {
-  Person.count().then((count) => {
-    let resString = `<p>Phonebook has info for ${count} people</p>`;
-    const currentDate = new Date();
-    resString += `${currentDate.toDateString()} ${currentDate.toTimeString()}`;
-    response.send(resString);
-  });
-});
+app.get('/api/blogs', (request, response) => {
+  Blog
+    .find({})
+    .then(blogs => {
+      response.json(blogs)
+    })
+})
 
-app.get('/api/persons', (request, response) => {
-  Person.find({}).then((people) => response.json(people));
-});
+app.post('/api/blogs', (request, response) => {
+  const blog = new Blog(request.body)
 
-app.get('/api/persons/:id', (request, response, next) => {
-  const {id} = request.params;
-  Person.findById(id).then((person) => {
-    if (person) {
-      response.json(person);
-    } else {
-      response.status(404).end();
-    }
-  })
-      .catch((error) => next(error));
-});
+  blog
+    .save()
+    .then(result => {
+      response.status(201).json(result)
+    })
+})
 
-app.post('/api/persons/', (request, response, next) => {
-  const {body} = request;
-
-  const person = new Person({
-    name: body.name,
-    number: body.number,
-  });
-
-  person.save().then((savedPerson) => {
-    response.json(savedPerson);
-  })
-      .catch((error) => next(error));
-});
-
-app.put('/api/persons/:id', (request, response, next) => {
-  const {name, number} = request.body;
-
-  Person.findByIdAndUpdate(
-      request.params.id,
-      {name, number},
-      {new: true, runValidators: true, context: 'query'},
-  )
-      .then((updatedPerson) => {
-        response.json(updatedPerson);
-      })
-      .catch((error) => next(error));
-});
-
-app.delete('/api/persons/:id', (request, response) => {
-  Person.findByIdAndDelete(request.params.id).then((result) => {
-    response.status(204).end();
-  })
-      .catch((error) => next(error));
-});
-
-const unknownEndpoint = (request, response) => {
-  response.status(404).send({error: 'unknown endpoint'});
-};
-app.use(unknownEndpoint);
-
-const errorhandler = (error, request, response, next) => {
-  console.error(error.message);
-  if (error.name === 'CastError') {
-    return response.status(400).send({error: 'malformatted id'});
-  } if (error.name === 'ValidationError') {
-    return response.status(400).json({error: error.message});
-  }
-  next(error);
-};
-app.use(errorhandler);
-
-// ##############################################################
-const {PORT} = process.env;
+const PORT = 3003
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+  console.log(`Server running on port ${PORT}`)
+})
