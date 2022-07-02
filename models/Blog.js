b9d4ec4ea8dde9d87eae71291f60aa872e7a0e75
Henrik Niskanen <h.niskanen99@gmail.com>
2022-07-02 12:38:39 +0300

    4.2 refactor

--- models/Blog.js
+++ models/Blog.js
@@ -1,38 +1,10 @@
 const mongoose = require('mongoose');
 
-const url = process.env.MONGODB_URI;
-console.log('connecting to', url);
-
-mongoose.connect(url)
-    .then((result) => {
-      console.log('connected to MongoDB');
-    }).catch((error) => {
-      console.log('error connecting to MongoDB:', error.message);
-    });
-
-const personSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    minlength: 3,
-    required: [true, 'name required'],
-  },
-  number: {
-    type: String,
-    minlength: 8,
-    required: [true, 'phone-number required'],
-    validate: {
-      validator: (num) => /^\d{2,3}-\d{5,}$/.test(num),
-      message: (props) => `${props.value} is not a valid phone number!`,
-    },
-  },
+const blogSchema = new mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number,
 });
 
-personSchema.set('toJSON', {
-  transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString();
-    delete returnedObject._id;
-    delete returnedObject.__v;
-  },
-});
-
-module.exports = mongoose.model('Person', personSchema);
+module.exports = mongoose.model('Blog', blogSchema);
